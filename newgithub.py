342dae50ecbadf37be1e855b229d889872c0edb1
Shaikumar724 <164379635+Shaikumar724@users.noreply.github.com>
2026-09-19 11:04:46 +0530

    control statements
    control statements

--- newgithub.py
+++ newgithub.py
@@ -1,1 +1,15 @@
-print("welcome")
+n = 1
+
+while n <= 5:
+    if n == 2:
+        print("Two")
+    elif n == 4:
+        n += 1
+        continue
+    elif n == 5:
+        break
+    else:
+        pass
+
+    print(n)
+    n += 1
