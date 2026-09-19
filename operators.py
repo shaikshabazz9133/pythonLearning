2ad3f03e7b242b7116c6ffe25a8cc065bbe7a93f
Shaikumar724 <164379635+Shaikumar724@users.noreply.github.com>
2026-09-19 11:39:12 +0530

    operators
    operators

---
+++ operators.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+
+print(a > b)
+print(a == b)
+print(a != b)
+
+print(a > 5 and b < 5)
+print(a > 5 or b > 5)
+print(not a > 5)
